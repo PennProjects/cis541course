a8bb20a7d0becd86f40fbce9ecd368a398fba101
Jal Panchal <jlpnchl@gmail.com>
2020-09-29 16:58:45 -0400

    added simple for loop

--- hw4_mqtt/submission/main.cpp
+++ hw4_mqtt/submission/main.cpp
@@ -3,10 +3,14 @@
 #include <mqtt/async_client.h>
 #include <mqtt/connect_options.h>
 #include <mqtt/ssl_options.h>
+#include <math.h>
+#include "time.h"
 
 #include "vc2gps.h"
 
 using namespace std;
+
+
 
 /**
  * A callback class for use with the main MQTT client.
@@ -29,11 +33,22 @@ public:
 
 int main(int argc, char* argv[]) {
 
-    double position = 50; // can take values from 0 to 100
-    RoadIdentifier road = RoadIdentifier::BC;
+    double position = 25; // can take values from 0 to 100
+    RoadIdentifier road = RoadIdentifier::AB;
     GPS_Coord c = GetLatLon(position, road);
-//    printf("lat %.9f\n", c.lat);
-//    printf("lon %.9f\n", c.lon);
+    printf("lat %.9f\n", c.lat);
+    printf("lon %.9f\n", c.lon);
+    cout << c.lon <<endl;
+
+
+    RoadIdentifier roadDE = RoadIdentifier::DE;
+    GPS_Coord e = GetLatLon(position, roadDE);
+    printf("lat_DE %.9f\n", e.lat);
+    printf("lon_DE %.9f\n", e.lon);
+
+    timespec sleep_time;
+    sleep_time.tv_sec = 1;
+    sleep_time.tv_nsec = 0;
 
     const string tb_uri = {"ssl://tb.precise.seas.upenn.edu:8883"};
     string client_id = {"client_id_1234"};
@@ -62,9 +77,26 @@ int main(int argc, char* argv[]) {
                                   "{\"device\":\"8660\",\"type\":\"Assignment#4\"}", 1, false);
     client.publish(msg)->wait();
 
-    auto msg2 = mqtt::make_message("v1/gateway/attributes",
-                                  "{\"8660\":{\"latitude\":39.955861,\"longitude\":-75.191443}}", 1, false);
-    client.publish(msg2)->wait();
+
+    for (int i =0; i<90; i= i+4 ) {
+
+        double position = 0+i;
+        RoadIdentifier roadDE = RoadIdentifier::DE;
+        GPS_Coord e = GetLatLon(position, roadDE);
+        printf("lat_DE %.9f\n", e.lat);
+        printf("lon_DE %.9f\n", e.lon);
+
+        string lat = to_string(e.lat);
+        string lon = to_string(e.lon);
+        string mq_message = "{\"8660\":{\"latitude\":" + lat + ",\"longitude\":" + lon + "}}";
+
+        auto msg2 = mqtt::make_message("v1/gateway/attributes",
+                                       mq_message, 1, false);
+        client.publish(msg2)->wait();
+
+        nanosleep(&sleep_time, NULL);
+//    client.publish(msg2)->wait();
+    }
 
 
 }
