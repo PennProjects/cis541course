6c613f6604c0fa85ee9dccd23ee212ee14293563
Jal Panchal <jlpnchl@gmail.com>
2020-09-30 09:49:39 -0400

    Amlost final_cleanup left

--- hw4_mqtt/submission/main.cpp
+++ hw4_mqtt/submission/main.cpp
@@ -17,8 +17,7 @@ using namespace std::chrono;
 
 
 
-
- //A callback class for use with the main MQTT client.
+// A callback class for use with the main MQTT client.
 class callback : public virtual mqtt::callback
 {
 public:
@@ -27,83 +26,106 @@ public:
         if (!cause.empty())
             std::cout << "\tcause: " << cause << std::endl;
     }
+//for debuting to see if message was received
 
-    void delivery_complete(mqtt::delivery_token_ptr tok) override {
-        std::cout << "\tDelivery complete for token: "
-                  << (tok ? tok->get_message_id() : -1) << std::endl;
-    }
+//     void delivery_complete(mqtt::delivery_token_ptr tok) override {
+//         std::cout << "\tDelivery complete for token: "
+//                   << (tok ? tok->get_message_id() : -1) << std::endl;
+//     }
 };
 
 
 int main(int argc, char* argv[]) {
 
 //  getting starting position of car at A
-    double start = 25; // Start at 0
+    double start_location = 0; // Start at 0
     RoadIdentifier road = RoadIdentifier::AB; //Select road AB
-    GPS_Coord gps = GetLatLon(start, road);
-    cout << "Starting Joiney at : " <<gps.lat<<","<<gps.lon<<endl;
+    GPS_Coord gps_start = GetLatLon(start_location, road);
+    cout << "Starting Joiney at : " <<gps_start.lat<<","<<gps_start.lon<<endl;
 
 //    setting loop wait time for updating location at 1s
-//    the wait after sending mqtt message is set to 20ms, based on repeated calculations of avg wait time
-//
+//    the wait after sending mqtt message is set to 10ms,
+//    based on repeated calculations of avg wait time, its about 10ms on codio
+//    to compensate for that, the wait time here is set to 990 ms
     timespec sleep_time;
     sleep_time.tv_sec = 0;
-    sleep_time.tv_nsec = 980000000;
+    sleep_time.tv_nsec = 990000000; //990 ms
 
+
+
+
+
+
+//     Settting MQTT connection parameters
     const string tb_uri = {"ssl://tb.precise.seas.upenn.edu:8883"};
     string client_id = {"client_id_1234"};
     string username = {"SHARED_DASHBOARD"};
     string password = {""};
 
-    // Init Client
+//     Init Client
     cout << "Initializing for server '" << tb_uri << "'..." << endl;
     mqtt::async_client client(tb_uri, client_id);
 
     callback cb;
     client.set_callback(cb);
 
-    // Username & Password
+//     Username & Password
     mqtt::connect_options connopts(username, password);
-    // Enable SSL
+//     Enable SSL
     mqtt::ssl_options sslopts;
     connopts.set_ssl(sslopts);
 
-    // Perform connection
+//     Perform connection
     client.connect(connopts)->wait();
-    cout << "  ...OK" << endl;
+    cout << "Client connected" << endl;
 
-    // publishing a message
-    auto msg = mqtt::make_message("v1/gateway/connect",
-                                  "{\"device\":\"8660\",\"type\":\"Assignment#4\"}", 1, false);
-    client.publish(msg)->wait();
+//     publishing a message
+    string mqtt_connection_topic = "v1/gateway/connect";
+    string register_car_msg = "{\"device\":\"8660\",\"type\":\"Assignment#4\"}";
 
+    auto register_msg = mqtt::make_message(mqtt_connection_topic, register_car_msg, 1, false);
+    client.publish(register_msg)->wait();
+
+
+
+
+
+//     To calibrate loop wait time
 //    auto start = high_resolution_clock::now();
-    for (int i =0; i<104; i= i+4 ) {
-        auto start = high_resolution_clock::now();
 
-        double position1 = 0+i;
-        RoadIdentifier roadDE1 = RoadIdentifier::DE;
-        GPS_Coord e = GetLatLon(position1, roadDE1);
-        printf("lat_DE %.9f\n", e.lat);
-        printf("lon_DE %.9f\n", e.lon);
+    GPS_Coord gps;
 
-        string lat = to_string(e.lat);
-        string lon = to_string(e.lon);
-        string mq_message = "{\"8660\":{\"latitude\":" + lat + ",\"longitude\":" + lon + "}}";
+//     update position by 4 meters every 1s to have a drive speed of 4 m/s
+    for (int i =0; i<101; i= i+4 ) {
 
-        auto msg2 = mqtt::make_message("v1/gateway/attributes",
-                                       mq_message, 1, false);
-        client.publish(msg2)->wait_for(19);
 
+//         getting updated gps location every second
+        double position = start_location +i;
+        gps = GetLatLon(position, road);
+        printf("The car is at %.9f , %.9f\n", gps.lat, gps.lon);
+
+//         message construction
+        string lat = to_string(gps.lat);
+        string lon = to_string(gps.lon);
+        string mqtt_locaction_message = "{\"8660\":{\"latitude\":" + lat + ",\"longitude\":" + lon + "}}";
+
+        string mqtt_attr_topic = "v1/gateway/attributes";
+
+//         pulishing location message to mqtt Dashboard
+        auto location_update_msg = mqtt::make_message(mqtt_attr_topic,mqtt_locaction_message, 1, false);
+        client.publish(location_update_msg)->wait_for(10); //wait for a 10ms Timeout for acknowlodgment from broker
+
+//         wait for 990 ms to ghave effective 1s update
         nanosleep(&sleep_time, NULL);
-//    client.publish(msg2)->wait();
-
-        auto stop = high_resolution_clock::now();
-
-        auto duration = duration_cast<microseconds>(stop - start);
-        cout << duration.count() << endl;
 
     }
+
+//         To calibrate loop wait time
+//         auto stop = high_resolution_clock::now();
+//         auto duration = duration_cast<microseconds>(stop - start);
+//         cout << duration.count() << endl;
+
+    printf("The car has reached its destination at B : %f , %f\n", gps.lat, gps.lon);
 
 
 }
